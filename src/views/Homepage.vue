4e9b68f5e5ddd3c64bcfef6b3b680aca8096579c
redtrib3 <anirudhkp2004@gmail.com>
2024-05-10 23:38:59 +0530

    major changes to homepage UI

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,22 +1,27 @@
 <template>
 
+    <div class="max-w-xl m-auto flex justify-center">
+        <div class="max-w-xl m-auto flex justify-center">
+        <main class="absolute inset-0 flex items-center justify-center">
+            <h1 class="lg:text-5xl text-3xl font-anonymouspro">
+                <span class="text-lime-400 hover:text-red-400 cursor-default" >[redtrib3</span>
 
-    <div class="container">
-        <main>
-            <h1 class="lg:text-4xl text-3xl font-anonymouspro">
-                <span class="text-lime-400">redtrib3</span>
                 <span class="text-yellow-200">@</span>
-                <span class="font-extrabold text-cyan-400">parrot</span>:~$
-                <span class="animate-ping font-extrabold">_</span>
-            </h1>
+                <span class="font-extrabold text-cyan-400">parrot]</span>
+                <span class="hidden md:inline">:~$ </span>
+                <span class="hidden md:inline animate-ping font-extrabold">_</span>
+            </h1>            
+            
         </main>
-
-        <div class="flex justify-center text-center mr-20 ml-14 mb-2 mt-2 text-gray-400 tracking-tight lg:tracking-wide">
-            Hi, I'm Anirudh, I'm interested in Cybersecurity, Privacy and Programming.
         </div>
 
     </div>
 
+    <div class="max-w-xl m-auto flex justify-center items-center absolute text-md font-anonymouspro inset-0 mt-20 pl-4">
+        <a href="https://github.com/redtrib3" class="pt-10 pr-4 lg:pr-10 font-bold hover:text-orange-400" target="_blank">[ Github ]</a>
+        <a href="https://linkedin.com/" class="pt-10 pr-4 lg:pr-10 font-bold hover:text-blue-400" target="_blank">[ Linkedin ]</a>
+        <a href="mailto:redtrib3@protonmail.com" class="pt-10 pr-4 lg:pr-10 hover:text-purple-400 font-bold" >[ Email ]</a>
+    </div>
 </template>
 
 <script>
@@ -26,9 +31,8 @@ export default {
 }
 </script>
 
-<style scoped>
 
-.container {
+ <!-- .container {
   max-width: 1000px;
   margin: auto;
   height: 72vh;
@@ -36,7 +40,5 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-}
+}  -->
 
-
-</style>
